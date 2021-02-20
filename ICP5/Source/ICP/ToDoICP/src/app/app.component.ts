77c42e7bd831dc3637495b565fdc172b95ff1af3
TomK97 <70316091+TomK97@users.noreply.github.com>
2021-02-20 15:05:48 +0100

    changes

--- ICP5/Source/ICP/ToDoICP/src/app/app.component.ts
+++ ICP5/Source/ICP/ToDoICP/src/app/app.component.ts
@@ -9,20 +9,32 @@ export class AppComponent {
 
   // define list of items
   items= [];
-
+  compltedItems= [];
   // Write code to push new item
-  submitNewItem() {
-
+  submitNewItem(event) {
+      this.items.push(event.taskName);
   }
 
   // Write code to complete item
-  completeItem() {
-
+  completeItem(item: string) {
+    const index: number = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+      this.compltedItems.push(item);
+    }
   }
 
   // Write code to delete item
-  deleteItem() {
-
+  deleteItem(item:string) {
+    let index: number = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    } else {
+      index = this.compltedItems.indexOf(item);
+      if (index !== -1) {
+        this.compltedItems.splice(index, 1);
+      }
+    }
   }
 
 }
